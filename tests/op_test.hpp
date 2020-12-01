69f15dcae0ead2c81bd33674e0f2dec64d7fe25e
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-01 12:09:40 -0800

    add tests

--- tests/op_test.hpp
+++ tests/op_test.hpp
@@ -3,6 +3,9 @@
 
 #include "gtest/gtest.h"
 #include "../composite/header/op.hpp"
+#include "../composite/header/add.hpp"
+#include "../iterator.hpp"
+#include "../visitor.hpp"
 
 //create iterator
 TEST(ClassOperandTest, OpIterator){
@@ -23,6 +26,26 @@ TEST(ClassOperandTest, OpGetRight){
     Base* one = new Op(1);
     
     EXPECT_TRUE(one->get_right() == nullptr);
+}
+
+//
+TEST(ClassOperandTest, OpVisitor){
+    Base* test = new Op(1);
+    Base* dummyNode = new Op(0);
+    Base* dummy = new Add(test, 0);
+
+    PreorderIterator* it = new PreorderIterator(dummy);
+
+    CountVisitor* visitor = new CountVisitor();
+    it->first();
+    while(!it->is_done()){
+        Base* currentExpression = it->current();
+        currentExpression->accept(visitor);
+        it->next();
+    }
+
+    EXPECT_EQ(visitor->op_count(), 1);
+    EXPECT_EQ(visitor->add_count(), 0);
 }
 
 
